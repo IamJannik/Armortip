b8df961d336131ac559222f938ac52770c2f48b4
IamJannik <jannik.ohme@gmail.com>
2025-12-17 18:16:30 +0100

    🪜PrepareTheLadder

--- src/client/java/net/bmjo/armortip/client/gui/tooltip/LeftTooltipPositioner.java
+++ src/client/java/net/bmjo/armortip/client/gui/tooltip/LeftTooltipPositioner.java
@@ -1,23 +1,25 @@
 package net.bmjo.armortip.client.gui.tooltip;
 
+import net.bmjo.armortip.util.ArmortipUtil;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.gui.tooltip.TooltipPositioner;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipPositioner;
+import org.jetbrains.annotations.NotNull;
 import org.joml.Vector2i;
 import org.joml.Vector2ic;
 
 @Environment(EnvType.CLIENT)
-public class LeftTooltipPositioner implements TooltipPositioner {
-    public static final TooltipPositioner INSTANCE = new LeftTooltipPositioner();
+public class LeftTooltipPositioner implements ClientTooltipPositioner {
+    public static final ClientTooltipPositioner INSTANCE = new LeftTooltipPositioner();
 
     private LeftTooltipPositioner() {
 
     }
 
     @Override
-    public Vector2ic getPosition(int screenWidth, int screenHeight, int mouseX, int mouseY, int width, int height) {
+    public @NotNull Vector2ic positionTooltip(int screenWidth, int screenHeight, int mouseX, int mouseY, int width, int height) {
         Vector2i start = new Vector2i(mouseX, mouseY).add(-12, -12);
-        start.add(-width, 0);
+        start.add(-width - ArmortipUtil.MARGIN * 2, 0);
         this.preventOverflow(screenHeight, start, width, height);
         return start;
     }
